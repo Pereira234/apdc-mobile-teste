ca2aeb12cf7b0af351078b29b302c301f955a0a1
David Santos <dgs.santos@campus.fct.unl.pt>
2021-07-06 17:48:49 +0100

    Profile pic attempt

--- app/src/main/java/com/example/lastapp/MainActivity.java
+++ app/src/main/java/com/example/lastapp/MainActivity.java
@@ -7,10 +7,14 @@ import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imgView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +24,9 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView bottomNavigationView = findViewById(R.id.BottomNavigationView);
         NavController navController = Navigation.findNavController(this,  R.id.fragmentContainerView);
         NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
+        imgView = findViewById(R.id.profilePicView);
+        Glide.with(this).load("https://cdn.discordapp.com/attachments/393483519055364117/860942284676399104/unknown.png").into(imgView);
 
     }
 }
--- app/src/main/java/com/example/lastapp/ProfileFragment.java
+++ app/src/main/java/com/example/lastapp/ProfileFragment.java
@@ -7,13 +7,18 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link FifthFragment#newInstance} factory method to
+ * Use the {@link ProfileFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class FifthFragment extends Fragment {
+public class ProfileFragment extends Fragment {
+
+    //ImageView imgView;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +29,7 @@ public class FifthFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public FifthFragment() {
+    public ProfileFragment() {
         // Required empty public constructor
     }
 
@@ -37,8 +42,8 @@ public class FifthFragment extends Fragment {
      * @return A new instance of fragment FifthFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static FifthFragment newInstance(String param1, String param2) {
-        FifthFragment fragment = new FifthFragment();
+    public static ProfileFragment newInstance(String param1, String param2) {
+        ProfileFragment fragment = new ProfileFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -53,6 +58,10 @@ public class FifthFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+        //imgView = findViewById(R.id.profilePicView);
+        //Glide.with(this).load("https://cdn.discordapp.com/attachments/393483519055364117/860942284676399104/unknown.png").into(imgView);
+
     }
 
     @Override
